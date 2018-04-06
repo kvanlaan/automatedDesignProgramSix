444b7947f059e3c8f8e541ef1178ebf96fdbe6aa
Xinyuan Zhang <xinyuanz@cs.utexas.edu>
2018-04-06 17:14:18 -0500

    iterator and insert

--- src/SPLList/SplList.java
+++ src/SPLList/SplList.java
@@ -6,9 +6,15 @@ import java.util.function.Consumer;
 
 public class SplList<Node extends SplListKey> implements Iterable<Node> {
 
-    public static boolean df = true;
-    public static boolean sort = false;
-    public static boolean single = false;
+    public static boolean df = false;
+    public static boolean sort = true;
+    public static boolean single = true;
+    
+    private final list mylist;
+    
+    public SplList() {
+        mylist = new list();
+    }
 
     public static void config(boolean d, boolean srt, boolean sing) {
         df = d;
@@ -26,7 +32,7 @@ public class SplList<Node extends SplListKey> implements Iterable<Node> {
 
     public node insert(Node n) {
         // TO DO
-        return null;
+        return mylist.insert(n);
     }
 
     @Override
@@ -37,7 +43,7 @@ public class SplList<Node extends SplListKey> implements Iterable<Node> {
     @Override
     public Iterator iterator() {
         // TO DO
-        return null;
+        return cursor();
     }
 
     @Override
@@ -57,7 +63,61 @@ public class SplList<Node extends SplListKey> implements Iterable<Node> {
 
         public node insert(Node n) {
             // TO DO
-            return null;
+            node curr_node = new node(n);
+            
+            if (head == null) {
+                head = curr_node;
+                tail = curr_node;
+                return curr_node;
+            }
+            
+            if (sort) {
+                if (head.me.getKey() >= n.getKey()) { // insert to the head
+                    curr_node.right = head;
+                    if (!single) {
+                        head.left = curr_node;
+                    }
+                    head = curr_node;
+                } else if (tail.me.getKey() < n.getKey()) { // insert to the tail
+                    tail.right = curr_node;
+                    if (!single) {
+                        curr_node.left = tail;
+                    }
+                    tail = curr_node;
+                } else { // insert to the middle
+                    node q = head;
+                    while (true) {
+                        if (q.right.me.getKey() >= n.getKey()) {
+                            break;
+                        }
+                        q = q.right;
+                    }
+                    if (!single) {
+                        // use double-sorted alg
+                        curr_node.right = q.right;
+                        q.right.left = curr_node;
+                        q.right = curr_node;
+                        curr_node.left = q;
+                    } else {
+                        // use single-sorted alg
+                        curr_node.right = q.right;
+                        q.right = curr_node;
+                    }
+                }
+            } else { // unsorted
+                // when inserting to the head, single/double-unsorted alg behave the same
+                curr_node.right = head;
+                if (!single) {
+                    head.left = curr_node;
+                }
+                head = curr_node;
+            }
+            
+            if (df) {
+                curr_node.delflag = false;
+            }
+            
+            return curr_node;
         }
 
         public void remove(node prev, node n) {
@@ -74,14 +134,20 @@ public class SplList<Node extends SplListKey> implements Iterable<Node> {
 
         public node(Node n) {
             // TO DO
+            me = n;
+            delflag = df;
+            left = null;
+            right = null;
         }
     }
 
     public class cursor implements Iterator<Node> {
         // TO DO -- add secret sauce
+        node pointer;
         
         public cursor() {
             // TO DO
+            pointer = mylist.head;
         }
 
         @Override
@@ -92,13 +158,15 @@ public class SplList<Node extends SplListKey> implements Iterable<Node> {
         @Override
         public boolean hasNext() {
             // TO DO
-            return true;
+            return pointer != null;
         }
 
         @Override
         public Node next() {
             // TO DO
-            return null;
+            node curr = pointer;
+            pointer =  pointer.right;
+            return curr.me;
         }
 
         @Override
